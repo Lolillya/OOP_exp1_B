b7675a115410ba4af6d43f79cab3f0feee83eb2d
Lolillya <SomeonesUsernam@gmail.com>
2023-08-10 19:41:08 +0800

    final output

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        double population = 1949400;
+        int year = 2024;
+
+        Console.WriteLine("Year" + "\t" + "Estimated Population");
+
+        for (int i = 0; i < 5; i++) {
+            population += population * 0.0218;
+            Console.WriteLine(year + "\t" + Convert.ToInt32(population));
+            year++;
+        }
+    }
+}
